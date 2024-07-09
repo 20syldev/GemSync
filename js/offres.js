44c44930f3d4b478c4ef3e6ff01a21b740f9ae99
20syldev <git@sylvain.pro>
2024-07-09 21:19:15 +0200

    Create offres.js

---
+++ js/offres.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Redirection vers la page de la langue de l'utilisateur
+    var manual = document.cookie.split('; ').find(row => row.startsWith('user_language_preference='))?.split('=')[1];
+
+    if (!manual) {
+        var FullUserLang = navigator.language || navigator.userLanguage;
+        var userLang = FullUserLang.slice(0, 2);
+
+        if (userLang === "en") {
+            if (window.location.href !== "https://en.gemsync.xyz/offres") {
+                //window.location.replace("https://en.gemsync.xyz/offres");
+            }
+        } else {
+            if (window.location.href !== "https://gemsync.xyz/offres") {
+                //window.location.replace("https://gemsync.xyz/offres");
+            }
+        }
+    }
+});
+
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+function toggleMenu(){
+    var  menuToggle = document.querySelector('.toggle');
+    var  menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
