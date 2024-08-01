f10d6d4d2b14b00b7e7cf17ad306aa8b0f5be4c5
20syl <77191035+20syldev@users.noreply.github.com>
2024-08-01 14:31:00 +0200

    Update offres.js
    Re-testing load

--- js/offres.js
+++ js/offres.js
@@ -50,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 requestAnimationFrame(scrollAnimation);
             }
         }
+        window.addEventListener("load", scrollToSection);
     }
 });
 
