0e7298d2d1a6191b8d66a5abeea2dc005ed671af
20syl <77191035+20syldev@users.noreply.github.com>
2024-08-01 14:20:35 +0200

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,19 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Redirection vers la page de la langue de l'utilisateur
-    var manual = document.cookie.split('; ').find(row => row.startsWith('user_language_preference='))?.split('=')[1];
+    const manual = document.cookie.split('; ').find(row => row.startsWith('user_language_preference='))?.split('=')[1];
 
     if (!manual) {
-        var FullUserLang = navigator.language || navigator.userLanguage;
-        var userLang = FullUserLang.slice(0, 2);
+        const userLang = (navigator.language || navigator.userLanguage).slice(0, 2);
+        const currentUrl = window.location.href;
 
-        if (userLang === "en") {
-            if (window.location.href !== "https://en.gemsync.xyz/404") {
-                window.location.replace("https://en.gemsync.xyz/404");
-            }
-        } else {
-            if (window.location.href !== "https://gemsync.xyz/404") {
-                window.location.replace("https://gemsync.xyz/404");
-            }
+        if (userLang === "en" && currentUrl !== "https://en.gemsync.xyz/404") {
+            window.location.replace("https://en.gemsync.xyz/404");
+        } else if (userLang !== "en" && currentUrl !== "https://gemsync.xyz/404") {
+            window.location.replace("https://gemsync.xyz/404");
         }
     }
 });
