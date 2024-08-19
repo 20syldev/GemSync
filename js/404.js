ab052dc9cfc3e1784538212600718800ba7cd7b0
20syldev <git@sylvain.pro>
2024-08-19 11:10:19 +0200

    Update 404.js

--- js/404.js
+++ js/404.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
     // Redirection vers la page de la langue de l'utilisateur
     const manual = document.cookie.split('; ').find(row => row.startsWith('user_language_preference='))?.split('=')[1];
 
@@ -6,10 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
         const userLang = (navigator.language || navigator.userLanguage).slice(0, 2);
         const currentUrl = window.location.href;
 
-        if (userLang === "en" && currentUrl !== "https://en.gemsync.xyz/404") {
-            window.location.replace("https://en.gemsync.xyz/404");
-        } else if (userLang !== "en" && currentUrl !== "https://gemsync.xyz/404") {
-            window.location.replace("https://gemsync.xyz/404");
+        if (userLang === 'en' && currentUrl !== 'https://en.gemsync.xyz/404') {
+            window.location.replace('https://en.gemsync.xyz/404');
+        } else if (userLang !== 'en' && currentUrl !== 'https://gemsync.xyz/404') {
+            window.location.replace('https://gemsync.xyz/404');
         }
     }
 });
