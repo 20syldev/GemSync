ada2e7cb14ec4c1991673ae2f2eb397d3bf3b7a7
20syldev <git@sylvain.pro>
2024-08-19 21:26:01 +0200

    Update offres.js

--- js/offres.js
+++ js/offres.js
@@ -1,3 +1,47 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js';
+import { getFirestore, doc, getDoc } from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js';
+
+// Configuration Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBeg4QPPuM9xghL1HwNiXEDbryyilpGmC8",
+    authDomain: "api-20syl.firebaseapp.com",
+    projectId: "api-20syl",
+    storageBucket: "api-20syl.appspot.com",
+    messagingSenderId: "18523544518",
+    appId: "1:18523544518:web:3c18c082bef03cce92b0c7"
+}
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const docRef = doc(db, 'infos', 'gemsync');
+
+// Fonction pour récupérer les données
+async function gemsync() {
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        const data = docSnap.data();
+        const reductions = [data.reduction1, data.reduction2, data.reduction3, data.reduction4];
+        const prices = [data.standard, data.plus, data.premium, data.pro].map((price, index) => 
+            (price * (1 - reductions[index] / 100)).toFixed(2)
+        );
+
+        prices.forEach((price, index) => {
+            const priceElement = document.getElementById(`price${index + 1}`);
+            const remiseElement = document.getElementById(`remise${index + 1}`);
+            priceElement.innerHTML = `${data[`type${index + 1}`]}€`;
+
+            if (reductions[index] !== '0') {
+                priceElement.innerHTML = `${price}€`;
+                remiseElement.style.display = 'inline-block';
+                remiseElement.textContent = `-${reductions[index]}%`;
+            }
+
+            document.getElementById(`paypal${index + 1}`).href = `https://www.paypal.com/paypalme/GemSync/${price}EUR`;
+        });
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     // Redirection vers la page de la langue de l'utilisateur
     const manual = document.cookie.split('; ').find(row => row.startsWith('user_language_preference='))?.split('=')[1];
@@ -13,7 +57,11 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    // Charger les promotions et les prix
+    gemsync()
+    
     // Gérer l'en-tête sticky
+    document.querySelector('header').classList.toggle('sticky', window.scrollY > 0);
     window.addEventListener('scroll', function() {
         document.querySelector('header').classList.toggle('sticky', window.scrollY > 0);
     });
@@ -41,6 +89,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// Fonction pour la version mobile
 function toggleMenu() {
     document.querySelector('.toggle').classList.toggle('active');
     document.querySelector('.menu').classList.toggle('active');
