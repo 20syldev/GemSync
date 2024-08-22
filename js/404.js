f480a759753bbc85965238973f2363ac2f565a53
20syldev <git@sylvain.pro>
2024-08-23 00:43:04 +0200

    Update 404.js
    Fix 404.js

--- js/404.js
+++ js/404.js
@@ -1,10 +1,10 @@
+// Récupérer la langue du navigateur et l'URL
+const userLang = (navigator.language || navigator.userLanguage).slice(0, 2);
+const url = userLang === 'en' ? 'https://en.gemsync.xyz/404' : 'https://gemsync.xyz/404';
+
 document.addEventListener('DOMContentLoaded', () => {
-    // Redirection vers la page de la langue de l'utilisateur
-    if (!document.cookie.includes('user_language_preference=')) {
-        if ((navigator.language || navigator.userLanguage).startsWith('en')) {
-            window.location.replace(window.location.href !== 'https://en.gemsync.xyz/404' ? 'https://en.gemsync.xyz/404' : window.location.href);
-        } else {
-            window.location.replace(window.location.href !== 'https://gemsync.xyz/404' ? 'https://gemsync.xyz/404' : window.location.href);
-        }
+    // Redirection et gestion de l'en-tête sticky
+    if (!document.cookie.includes('user_language_preference=') && window.location.href !== url) {
+        window.location.replace(url);
     }
 });
