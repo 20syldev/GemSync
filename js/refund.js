555de081325f351e45c01ac226fe8f33715d5d94
20syldev <git@sylvain.pro>
2024-09-01 17:37:03 +0200

    Create refund.js

---
+++ js/refund.js
@@ -0,0 +1,22 @@
+// Récupérer la langue du navigateur et l'URL
+const userLang = (navigator.language || navigator.userLanguage).slice(0, 2);
+const url = userLang === 'en' ? 'https://en.gemsync.xyz/refund' : 'https://gemsync.xyz/refund';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Redirection et gestion de l'en-tête sticky
+    if (!document.cookie.includes('user_language_preference=') && window.location.href !== url) {
+        window.location.replace(url);
+    }
+    document.querySelector('header').classList.toggle('sticky', window.scrollY > 0);
+});
+
+// Gérer l'en-tête sticky
+window.addEventListener('scroll', () => {
+    document.querySelector('header').classList.toggle('sticky', window.scrollY > 0);
+});
+
+// Fonction pour la version mobile
+function toggleMenu() {
+    document.querySelector('.toggle').classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('active');
+}
